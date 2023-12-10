f0d89d0b667f68aca46a0fee0d46f97e4d6691b5
Lea Rosema <lea@lea.codes>
2023-12-10 23:03:03 +0100

    feat: disable buttons when action not possible

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop'
+
 import { ResourceTileComponent } from '../../components/resource-tile/resource-tile.component';
-import { ActionCreatorProps, Store } from '@ngrx/store';
+import { Store } from '@ngrx/store';
 import { selectCycle, selectFacilities, selectResources } from '../../state/game/game.selectors';
-import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { gameActions } from '../../state/game/game.actions';
 import { FACILITY } from '../../models/facilities.model';
-import { Subscription, interval } from 'rxjs';
+import { Observable, Subscription, combineLatest, concat, find, interval, last, map, of, switchMap, take } from 'rxjs';
+import { RESOURCE } from '../../models/inventory.model';
 
 export interface FacilityAction {action: any; title: string }
 
@@ -57,6 +58,32 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   sellCoffee() {
     this.store.dispatch(gameActions.sellCoffee());
+  }
+
+  canWork(facility: string): Observable<boolean> {
+    return combineLatest([this.resources$]).pipe(
+      map(([inventory]) => {
+        switch (facility) {
+          case FACILITY.plant: return false;
+          case FACILITY.roaster: return inventory[RESOURCE.bean] > 0;
+          case FACILITY.grinder: return inventory[RESOURCE.roastedBean] > 0;
+          case FACILITY.brewer: return inventory[RESOURCE.water] > 0 && inventory[RESOURCE.groundCoffee] > 0
+        }
+        return true;
+      })
+    );
+  }
+
+  canUpgrade(): Observable<boolean> {
+    return combineLatest([this.resources$]).pipe(map(([inventory]) => {
+      return inventory[RESOURCE.cash] > 100;
+    }));
+  }
+
+  canSell(): Observable<boolean> {
+    return combineLatest([this.resources$]).pipe(map(([inventory]) => {
+      return inventory[RESOURCE.coffee] > 0;
+    }));
   }
 
 }
